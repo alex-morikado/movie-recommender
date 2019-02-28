5e5cd0774006c3801a4cced4bdbca155e305b2e1
alex-morikado <asmorikado+git@gmail.com>
2019-02-27 19:15:28 -0600

    minor updates to remove duplicate data

--- omdb_downloader.py
+++ omdb_downloader.py
@@ -30,7 +30,7 @@ def reduce_data(movie, fields=["Title", "Year", "Rated", "Runtime", "Genre", "Di
     if reduced.get("Genre", False):
         reduced["Genre"] = reduced["Genre"].split(", ")
     if reduced.get("Writer", False):
-        reduced["Writer"] = [writer[0:writer.find('(')].strip() for writer in reduced["Writer"].split(", ")]
+        reduced["Writer"] = list(set([writer[0:writer.find('(')].strip() for writer in reduced["Writer"].split(", ")]))
     if reduced.get("Actors", False):
         reduced["Actors"] = reduced["Actors"].split(", ")
     if reduced.get("Language", False):
@@ -46,7 +46,7 @@ def format_attribute(attribute):
 def write_krf(entity, id, file):
     file.write('(isa ' + 'tt' + id + ' Movie-CW)\n')
     for key in entity:
-        pred_and_id = '(' + omdbCycMapping.get(key, key) + ' tt' + str(id)
+        pred_and_id = '(' + omdbCycMapping.get(key, "movie" + key) + ' tt' + str(id)
         if type(entity[key]) == list:
             for attribute in entity[key]:
                 file.write(pred_and_id + ' ' + format_attribute(attribute) + ')\n')
@@ -59,7 +59,7 @@ def write_file(max_lines):
     link_file = open(LINK_FILE_PATH, 'r')
     output_file = open(OUTPUT_FILENAME, 'w')
 
-    output_file.write('(in-microtheory Movie-Recommender)')
+    output_file.write('(in-microtheory Movie-Recommender)\n\n\n')
     #skip the first line as it has the headers
     link_file.readline()
 
